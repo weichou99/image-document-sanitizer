2030393d812acd88f21fb1a10f414372611b9c03
weichou99 <weichou99@gmail.com>
2019-05-10 15:49:53 +0800

    create project

---
+++ image-document-sanitizer/src/main/java/cc/image_document_sanitizer/App.java
@@ -0,0 +1,13 @@
+package cc.image_document_sanitizer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
